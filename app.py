315a4288b41ce04038e8a0cc128752ec970b29c0
andreamartz <14165788+andreamartz@users.noreply.github.com>
2020-09-02 13:09:55 -0500

    Add comment about the Debug Toolbar showing redirects

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly_db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 app.config['SECRET_KEY'] = "supersecretcodefordebugtoolbar"
+# Having the Debug Toolbar show redirects explicitly is often useful.  To turn it on, comment out this line:
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 debug = DebugToolbarExtension(app)
 
