393c52359825a28f1b64c9425d59f2734165f842
andreamartz <14165788+andreamartz@users.noreply.github.com>
2020-09-11 11:37:21 -0500

    Add PostTag model

--- models.py
+++ models.py
@@ -113,3 +113,13 @@ class Tag(db.Model):
 
 
 class PostTag(db.Model):
+    """PostTag for joins"""
+
+    __tablename__ = "posts_tags"
+
+    post_id = db.Column(db.Integer,
+                        db.ForeignKey('posts.id'), primary_key=True)
+
+    tag_id = db.Column(db.Integer,
+                       db.ForeignKey('tags.id'),
+                       primary_key=True)
